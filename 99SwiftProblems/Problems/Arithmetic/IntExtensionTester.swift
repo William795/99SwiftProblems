e9555b28e101d21a60080cad8e2a42eec4d44510
William Moody <oswin795@outlook.com>
2019-09-11 17:04:49 -0600

    adds soloutions to problems 31 and 32

---
+++ 99SwiftProblems/Problems/Arithmetic/IntExtensionTester.swift
@@ -0,0 +1,9 @@
+//
+//  IntExtensionTester.swift
+//  99SwiftProblems
+//
+//  Created by William Moody on 9/10/19.
+//  Copyright © 2019 William Moody. All rights reserved.
+//
+
+import Foundation
